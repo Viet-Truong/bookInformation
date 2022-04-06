a22003b75d930cff98526d216b2498a13cb4c1ab
Viet Truong <viettruong0825@gmail.com>
2022-04-06 21:46:47 +0700

    first commit

---
+++ BookInformation/src/Book/Book.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Book;
+
+/**
+ *
+ * @author AD
+ */
+public class Book {
+    private int id;
+    private String name;
+    private double money;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+    
+}
